4b2b95c0ea1ff66f7bd6f5a6f055a10e7b59496b
Nikita <palnov.nikita@gmail.com>
2023-05-05 15:35:59 +0300

    Changed score system

--- Score.py
+++ Score.py
@@ -2,17 +2,19 @@ from Utils import SCORES_FILE_NAME
 from os.path import exists
 
 def add_score(difficulty):
-    POINTS_OF_WINNING = (difficulty * 3) + 5
-    contents = None
-    if exists (SCORES_FILE_NAME):
-        file = open(SCORES_FILE_NAME, 'r')
-        contents = file.read()
-        file.close()
-    if contents:
-        current_score = int(contents) + POINTS_OF_WINNING
-    else:
-        current_score = POINTS_OF_WINNING
-    file = open(SCORES_FILE_NAME, 'w')
-    file.write(str(current_score))
-    file.close()
-    return current_score
+    scores = open(SCORES_FILE_NAME, 'a')
+    scores.close()
+
+    with open(SCORES_FILE_NAME, 'r', encoding='utf8') as my_cur_score:
+        current_score = my_cur_score.readline()
+
+    with open(SCORES_FILE_NAME, 'w', encoding='utf8') as new_cur_score:
+        POINTS_OF_WINNING = (difficulty * 3) + 5
+        print(POINTS_OF_WINNING)
+        if current_score == '':
+            new_score = 0 + POINTS_OF_WINNING
+            total_score = new_cur_score.write(str(new_score))
+        else:
+            new_score = int(current_score) + POINTS_OF_WINNING
+            total_score = new_cur_score.write(str(new_score))
+        return total_score
